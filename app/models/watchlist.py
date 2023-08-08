5c6ec8793532223a3cbd4bf5fa5b4dc43c3ef5a8
BryantS <bryantstine@yahoo.com>
2023-08-08 16:21:24 -0700

    watchlists import2

--- app/models/watchlist.py
+++ app/models/watchlist.py
@@ -1,6 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .user import User
-from .movie_watchlist import MovieWatchlist 
+from .moviewatchlist import MovieWatchlist
 from datetime import datetime
 
 class Watchlist(db.Model):
