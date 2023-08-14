b9b66846e7a4b302db12186ed5bae2449074cabf
BryantS <bryantstine@yahoo.com>
2023-08-14 10:09:41 -0700

    edit movie thunk edited to have errorobj

--- app/__init__.py
+++ app/__init__.py
@@ -31,7 +31,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(movie_routes, url_prefix='/api/movies')
-app.register_blueprint(movie_routes, url_prefix='/api/reviews')
+app.register_blueprint(review_routes, url_prefix='/api/reviews')
 db.init_app(app)
 Migrate(app, db)
 
